6807ee81ebbfc581ca96d69d85c40dcbd4133cdf
Md Nazim Mahmud Khan <mdnazimmahmudkhan@gmail.com>
2024-12-09 00:36:25 +0600

    mongodb added using typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://127.0.0.1:27017/pim',
+      database: 'pim',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      entities: [User],
+    }),
+    
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, ObjectIdColumn, ObjectId, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @ObjectIdColumn()
+  id: ObjectId;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+}
